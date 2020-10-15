f445aa64c52b0415355d1cbea86d879e12d15f09
Stasys Meclazcke <aeip@live.com>
2020-10-15 10:35:39 -0500

    smaller images

--- musicapp.js
+++ musicapp.js
@@ -18,55 +18,55 @@ const itunesSearch = (search) => {
     $.getJSON(url,
 		function (data) {
             for (let i = 0; i < data.results.length; i++) {
-							resultsArr = data.results;
-							$resultDiv = $(
-								'<div class="resultDiv" onclick="tappedDiv(this)">'
-							);
-							$artwork = $(
-								'<img class="artwork" src="' +
-									resultsArr[i].artworkUrl100 +
-									'">'
-							);
-							$song = $('<h3 class="artist">').text(resultsArr[i].trackName);
-							$artist = $('<p class="artist">').text(resultsArr[i].artistName);
-							$audioPlayer = $('<audio class="musicplayer" controls>');
-							$preview = $(
-								'<source src="' +
-									resultsArr[i].previewUrl +
-									'" type="audio/mpeg">'
-							);
-							$('#display').append($resultDiv);
-							$resultDiv.append($artwork);
-							$resultDiv.append($song);
-							$resultDiv.append($artist);
-							$('.musicplayer').hide();
-							$resultDiv.append($audioPlayer);
-							$audioPlayer.append($preview);
-							$('.musicplayer').hide();
-							Vibrant.from(resultsArr[i].artworkUrl100)
-								.getPalette()
-                                .then((palette) => {
-                                    function setColor() {
-                                        var random = Math.floor(Math.random() * 6);
-                                        if (random === 1) {
-                                            $resultDiv.css('background-color', 'rgb(' + palette.Vibrant._rgb[0] + ',' + palette.Vibrant._rgb[1] + ',' + palette.Vibrant._rgb[2] + ')');
-                                        } else if (random === 2) {
-                                            $resultDiv.css('background-color', 'rgb(' + palette.Vibrant._rgb[0] + ',' + palette.LightVibrant._rgb[1] + ',' + palette.LightVibrant._rgb[2] + ')');
-                                        } else if (random === 3) {
-                                            $resultDiv.css('background-color', 'rgb(' + palette.DarkVibrant._rgb[0] + ',' + palette.DarkVibrant._rgb[1] + ',' + palette.DarkVibrant._rgb[2] + ')');
-                                        } else if (random === 4) {
-                                            $resultDiv.css('background-color', 'rgb(' + palette.Muted._rgb[0] + ',' + palette.Muted._rgb[1] + ',' + palette.Muted._rgb[2] + ')');
-                                        } else if (random === 5) {
-                                            $resultDiv.css('background-color', 'rgb(' + palette.LightMuted._rgb[0] + ',' + palette.LightMuted._rgb[1] + ',' + palette.LightMuted._rgb[2] + ')');
-                                        } else if (random === 6) {
-                                            $resultDiv.css('background-color', 'rgb(' + palette.DarkMuted._rgb[0] + ',' + palette.DarkMuted._rgb[1] + ',' + palette.DarkMuted._rgb[2] + ')');
-                                        } else if (random === 0) {
-                                            $resultDiv.css('background-color', 'rgb(' + palette.Vibrant._rgb[0] + ',' + palette.Vibrant._rgb[1] + ',' + palette.Vibrant._rgb[2] + ')');
-                                        }
-                                    }
-                                    setInterval(setColor, 5000);
-                                });
-						}
+                resultsArr = data.results;
+                $resultDiv = $(
+                    '<div class="resultDiv" onclick="tappedDiv(this)">'
+                );
+                $artwork = $(
+                    '<img class="artwork" src="' +
+                        resultsArr[i].artworkUrl100 +
+                        '">'
+                );
+                $song = $('<h3 class="artist">').text(resultsArr[i].trackName);
+                $artist = $('<p class="artist">').text(resultsArr[i].artistName);
+                $audioPlayer = $('<audio class="musicplayer" controls>');
+                $preview = $(
+                    '<source src="' +
+                        resultsArr[i].previewUrl +
+                        '" type="audio/mpeg">'
+                );
+                $('#display').append($resultDiv);
+                $resultDiv.append($artwork);
+                $resultDiv.append($song);
+                $resultDiv.append($artist);
+                $('.musicplayer').hide();
+                $resultDiv.append($audioPlayer);
+                $audioPlayer.append($preview);
+                $('.musicplayer').hide();
+                Vibrant.from(resultsArr[i].artworkUrl100)
+                    .getPalette()
+                    .then((palette) => {
+                        function setColor() {
+                            var random = Math.floor(Math.random() * 6);
+                            if (random === 1) {
+                                $resultDiv.css('background-color', 'rgb(' + palette.Vibrant._rgb[0] + ',' + palette.Vibrant._rgb[1] + ',' + palette.Vibrant._rgb[2] + ')');
+                            } else if (random === 2) {
+                                $resultDiv.css('background-color', 'rgb(' + palette.Vibrant._rgb[0] + ',' + palette.LightVibrant._rgb[1] + ',' + palette.LightVibrant._rgb[2] + ')');
+                            } else if (random === 3) {
+                                $resultDiv.css('background-color', 'rgb(' + palette.DarkVibrant._rgb[0] + ',' + palette.DarkVibrant._rgb[1] + ',' + palette.DarkVibrant._rgb[2] + ')');
+                            } else if (random === 4) {
+                                $resultDiv.css('background-color', 'rgb(' + palette.Muted._rgb[0] + ',' + palette.Muted._rgb[1] + ',' + palette.Muted._rgb[2] + ')');
+                            } else if (random === 5) {
+                                $resultDiv.css('background-color', 'rgb(' + palette.LightMuted._rgb[0] + ',' + palette.LightMuted._rgb[1] + ',' + palette.LightMuted._rgb[2] + ')');
+                            } else if (random === 6) {
+                                $resultDiv.css('background-color', 'rgb(' + palette.DarkMuted._rgb[0] + ',' + palette.DarkMuted._rgb[1] + ',' + palette.DarkMuted._rgb[2] + ')');
+                            } else if (random === 0) {
+                                $resultDiv.css('background-color', 'rgb(' + palette.Vibrant._rgb[0] + ',' + palette.Vibrant._rgb[1] + ',' + palette.Vibrant._rgb[2] + ')');
+                            }
+                        }
+                        setInterval(setColor, 5000);
+                    });
+            }
 		}
 	);
 }
